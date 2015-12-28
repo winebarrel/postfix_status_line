fe83bed853143db454d3690b6ad07d2f51a13373
Genki Sugawara <sugawara@cookpad.com>
2015-12-28 13:50:55 +0900

    Fix spec

--- spec/postfix_status_line_hash_spec.rb
+++ spec/postfix_status_line_hash_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 if ENV['DISABLE_OPENSSL'] != '1'
   describe PostfixStatusLine do
+    let(:options) do
+      {mask: true}
+    end
+
+    subject { PostfixStatusLine.parse(status_line, options) }
+
     context 'with hash' do
       let(:status_line) do
        'Feb 27 09:02:37 MyHOSTNAME postfix/smtp[26490]: D53A72713E5: to=<myemail@bellsouth.net>, relay=gateway-f1.isp.att.net[204.127.217.16]:25, conn_use=2, delay=0.57, delays=0.11/0.03/0.23/0.19, dsn=2.0.0, status=sent (250 ok ; id=20120227140036M0700qer4ne)'
