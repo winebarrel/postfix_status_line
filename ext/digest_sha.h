56f82cab784c639e5368a9bd0c22dedfd4c02e52
Genki Sugawara <sugawara@cookpad.com>
2017-01-18 20:08:20 +0900

    Add sha algorithms

---
+++ ext/digest_sha.h
@@ -0,0 +1,40 @@
+#ifndef __DIGEST_SHA_H__
+#define __DIGEST_SHA_H__
+
+#ifndef SHA224_CTX
+#define SHA224_CTX SHA256_CTX
+#endif
+
+#ifndef SHA384_CTX
+#define SHA384_CTX SHA512_CTX
+#endif
+
+#define DEFINE_SHA_FUNCTIONS(SHA_ALGO) \
+  static void sha##SHA_ALGO##_to_str(unsigned char *hash, char buf[]) { \
+    int i; \
+    for (i = 0; i < SHA##SHA_ALGO##_DIGEST_LENGTH; i++) { \
+      snprintf(buf + (i * 2), 3, "%02x", hash[i]); \
+    } \
+  } \
+  static bool digest_sha##SHA_ALGO(char *str, char *salt, size_t salt_len, char buf[]) { \
+    SHA##SHA_ALGO##_CTX ctx; \
+    unsigned char hash[SHA##SHA_ALGO##_DIGEST_LENGTH]; \
+    if (SHA##SHA_ALGO##_Init(&ctx) != 1) { \
+      return false; \
+    } \
+    if (SHA##SHA_ALGO##_Update(&ctx, str, strlen(str)) != 1) { \
+      return false; \
+    } \
+    if (salt != NULL) { \
+      if (SHA##SHA_ALGO##_Update(&ctx, salt, salt_len) != 1) { \
+        return false; \
+      } \
+    } \
+    if (SHA##SHA_ALGO##_Final(hash, &ctx) != 1) { \
+      return false; \
+    } \
+    sha##SHA_ALGO##_to_str(hash, buf); \
+    return true; \
+  }
+
+#endif // __DIGEST_SHA_H__
--- lib/postfix_status_line.rb
+++ lib/postfix_status_line.rb
@@ -2,12 +2,15 @@ require 'postfix_status_line/version'
 require 'postfix_status_line_core'
 
 module PostfixStatusLine
+  SHA_ALGORITHM = %w(1 224 256 384 512)
+
   def parse(str, options = {})
     mask = options.has_key?(:mask) ? options[:mask] : true
     hash = options[:hash]
     salt = options[:salt]
     parse_time = options[:parse_time]
-    PostfixStatusLine::Core.parse(str, mask, hash, salt, parse_time)
+    sha_algo = options[:sha_algorithm]
+    PostfixStatusLine::Core.parse(str, mask, hash, salt, parse_time, sha_algo)
   end
   module_function :parse
 end
